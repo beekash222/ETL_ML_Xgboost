3a35e26ef01f710db38db16525bb508be012ce81
beekash222 <39214665+beekash222@users.noreply.github.com>
2018-05-15 07:56:50 +0530

    Update Claim_Xgboost.R

--- Claim_Xgboost.R
+++ Claim_Xgboost.R
@@ -72,3 +72,45 @@ barplot(table(test_new$result),col = c("forestgreen", "deepskyblue"))
 test_new[test_new$result == "Unmatch",1:8]
 
 View(test)
+
+###########learning curve############
+
+# create empty data frame 
+learnCurve <- data.frame(m = integer(nrow(train)),
+                         trainRMSE = integer(nrow(train)),
+                         cvRMSE = integer(nrow(train)))
+
+
+metric <- "RMSE"
+
+# loop over training examples
+for (i in 3:nrow(sparse_matrix)) {
+  learnCurve$m[i] <- i
+  # train learning algorithm with size i
+  model <- xgboost(data = sparse_matrix[1:i,],
+                   label = train$COB_SAVINGS_AMT[1:i],
+                   max.depth = 4,
+                   eta = 1,      # set 0, 0.5 and 1
+                   nthread = 2,
+                   nround = 100,  
+                   objective = "reg:linear")
+    # train learning algorithm with size i       
+  learnCurve$trainRMSE[i] <- model$evaluation_log$train_rmse
+  
+  # use trained parameters to predict on test data
+  prediction <- predict(model, newdata = sparse_matrix_test)
+  rmse <- postResample(prediction, test$COB_SAVINGS_AMT)
+  learnCurve$cvRMSE[i] <- rmse[1]
+}
+
+pdf("LearningCurve.pdf", width = 7, height = 7, pointsize=12)
+
+# plot learning curves of training set size vs. error measure
+# for training set and test set
+plot(log(learnCurve$trainRMSE),type = "o",col = "red", xlab = "Training set size",
+     ylab = "Error (RMSE)", main = "Linear Model Learning Curve")
+lines(log(learnCurve$cvRMSE), type = "o", col = "blue")
+legend('topright', c("Train error", "Test error"), lty = c(1,1), lwd = c(2.5, 2.5),
+       col = c("red", "blue"))
+
+dev.off()
